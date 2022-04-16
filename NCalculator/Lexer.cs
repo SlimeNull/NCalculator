965cc36457b2d4ab320e44d1e13f81614a457294
SlimeNull <slimenull@outlook.com>
2022-04-16 14:05:33 +0800

    基于递归下降的语法分析

---
+++ NCalculator/Lexer.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null.Calculator
+{
+    public class Lexer
+    {
+        private readonly TextReader baseReader;
+
+        public TextReader BaseReader => baseReader;
+        public Lexer(TextReader tr)
+        {
+            baseReader = tr ?? throw new ArgumentNullException(nameof(tr));
+        }
+
+        public IEnumerable<Token> Lex()
+        {
+            while (true)
+            {
+                var ch = baseReader.Read();
+                char cch = (char)ch;
+
+                if (ch == -1)
+                {
+                    yield break;
+                }
+
+                if (char.IsWhiteSpace(cch))
+                {
+                    continue;
+                }
+                else if (char.IsLetter(cch))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(cch);
+
+                    while (true)
+                    {
+                        ch = baseReader.Peek();
+                        cch = (char)ch;
+
+                        if (char.IsLetterOrDigit(cch))
+                        {
+                            baseReader.Read();
+                            sb.Append(cch);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    yield return new Token(TokenKind.Identifier, sb.ToString());
+                    continue;
+                }
+                else if (char.IsDigit(cch))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.Append(cch);
+
+                    while (ch != -1)
+                    {
+                        ch = baseReader.Peek();
+                        cch = (char)ch;
+                        if (ch >= '0' && ch <= '9')
+                        {
+                            baseReader.Read();
+                            sb.Append(cch);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (cch == '.')
+                    {
+                        ch = baseReader.Read();  // skip '.'
+                        sb.Append(cch);
+                        while (ch != -1)
+                        {
+                            ch = baseReader.Peek();
+                            cch = (char)ch;
+                            if (ch >= '0' && ch <= '9')
+                            {
+                                baseReader.Read();
+                                sb.Append(cch);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    if (cch == 'e' || cch == 'E')
+                    {
+                        baseReader.Read();  // skip 'e'
+                        sb.Append(cch);
+
+                        ch = baseReader.Read();
+                        if (ch >= '0' || ch <= '9' || ch == '+' || ch == '-')
+                        {
+                            baseReader.Read();
+                            sb.Append((char)ch);
+                        }
+
+                        while (ch != -1)
+                        {
+                            ch = baseReader.Peek();
+                            cch = (char)ch;
+                            if (ch >= '0' && ch <= '9')
+                            {
+                                baseReader.Read();
+                                sb.Append(cch);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    
+                    yield return new Token(TokenKind.Number, sb.ToString());
+                    continue;
+                }
+                else
+                {
+                    switch (ch)
+                    {
+                        case '+':
+                            yield return new Token(TokenKind.Plus, null);
+                            break;
+                        case '-':
+                            yield return new Token(TokenKind.Sub, null);
+                            break;
+                        case '*':
+                            if (baseReader.Peek() == '*')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.Pow, null);
+                            }
+                            else
+                            {
+                                yield return new Token(TokenKind.Mul, null);
+                            }
+                            break;
+                        case '/':
+                            yield return new Token(TokenKind.Div, null);
+                            break;
+                        case '(':
+                            yield return new Token(TokenKind.LParen, null);
+                            break;
+                        case ')':
+                            yield return new Token(TokenKind.RParen, null);
+                            break;
+                        case '?':
+                            yield return new Token(TokenKind.Question, null);
+                            break;
+                        case ':':
+                            yield return new Token(TokenKind.Colon, null);
+                            break;
+                        case ',':
+                            yield return new Token(TokenKind.Comma, null);
+                            break;
+                        case '^':
+                            yield return new Token(TokenKind.Pow, null);
+                            break;
+                        case '>':
+                            if (baseReader.Peek() == '=')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.GtrEq, null);
+                            }
+                            if (baseReader.Peek() == '<')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.NoEq, null);
+                            }
+                            else
+                            {
+                                yield return new Token(TokenKind.Gtr, null);
+                            }
+                            break;
+                        case '<':
+                            if (baseReader.Peek() == '=')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.LssEq, null);
+                            }
+                            if (baseReader.Peek() == '>')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.NoEq, null);
+                            }
+                            else
+                            {
+                                yield return new Token(TokenKind.Lss, null);
+                            }
+                            break;
+                        case '=':
+                            if (baseReader.Peek() == '=')
+                            {
+                                baseReader.Read();
+                                yield return new Token(TokenKind.Eq, null);
+                            }
+                            else
+                            {
+                                yield return new Token(TokenKind.Eq, null);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ NCalculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Null.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator    By SlimeNull, 2022");
+            Console.WriteLine("  Funs: abs ceil floor min max ln log log10 sqrt exp sin cos tan asin acos atan sinh cosh tanh");
+            Console.WriteLine("  Opts: (...) * / % ** ^ + - > >= < <= == != & | .?.:.");
+            Console.WriteLine();
+            
+            while (true)
+            {
+                Console.Write(">>> ");
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                    return;
+
+                Token[] tokens = new Lexer(new StringReader(input)).Lex().ToArray();
+                Expr expr = new Parser(tokens).Parse();
+                Console.WriteLine(expr.Eval());
+            }
+        }
+    }
+}
